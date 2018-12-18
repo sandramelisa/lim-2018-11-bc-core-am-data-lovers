efc308b8219748c1bbc68c1787fc2b238b34cd4f
sandramelisa <sanchezfalcons@gmail.com>
2018-12-18 08:33:14 -0500

    mostrar imagen,id,nombre de pokemon con template

--- src/data.js
+++ src/data.js
@@ -1,8 +1,41 @@
-// esta es una función de ejemplo
+ // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+*/
+
+let arrayPokemon = POKEMON.pokemon;
+const containerList = document.getElementById('container-list');
+
+const crearTemplateDeCard = (data) => {
+  let templateListOfCards = '';
+  // recorremos nuestro array con forEach
+  data.forEach((personaje) => {
+    // creamos un template(string) por cada elemento del array
+    const card = `
+      <div class="card-link">
+        <article class="blog-card">
+          <img class="post-image" src="${ personaje.img }" />
+          <div class="article-details">
+            <h4 class="post-category">${ personaje.id }</h4>
+            <h3 class="post-title">${ personaje.name }</h3>
+            <p class="post-description">
+              Probabilidad ${ personaje.avg_spawns}
+            </p>
+            <p class="post-author">Debilidades ${ personaje.weaknesses }</p>
+          </div>
+        </article>
+      </div>
+    `;
+    // vamos concatenando cada li
+    templateListOfCards += card;
+  })
+  containerList.innerHTML = templateListOfCards;
+}
+
+// ejecuntado funciona para pintar la data en mi html
+crearTemplateDeCard(POKEMON.pokemon)
