ca6ac4d91cb1b3d62da7d51ebb279ba50fee7db3
sandramelisa <sanchezfalcons@gmail.com>
2018-12-19 16:40:21 -0500

    pokemon inline table,figma historia de usuario1

--- src/data.js
+++ src/data.js
@@ -17,19 +17,14 @@ const crearTemplateDeCard = (data) => {
   data.forEach((personaje) => {
     // creamos un template(string) por cada elemento del array
     const card = `
-      <div class="card-link">
-        <article class="blog-card">
-          <img class="post-image" src="${ personaje.img }" />
-          <div class="article-details">
-            <h4 class="post-category">${ personaje.id }</h4>
-            <h3 class="post-title">${ personaje.name }</h3>
-            <p class="post-description">
-              average spawn ${ personaje.avg_spawns}
-            </p>
-            <p class="post-author">weaknesses ${ personaje.weaknesses }</p>
-          </div>
-        </article>
-      </div>
+            <div class="card-link">
+            <img class="post-image" src="${ personaje.img }" />
+            <label class="post-category">${ personaje.id }</label>
+            <label class="post-title">${ personaje.name }</label>
+            <label class="post-description">
+              average spawn ${ personaje.avg_spawns}</label>
+            <label class="post-author">weaknesses ${ personaje.weaknesses }</label>
+            </div>
     `;
     // vamos concatenando cada li
     templateListOfCards += card;
